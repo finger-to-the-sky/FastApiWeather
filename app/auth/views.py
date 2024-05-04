82547c73a5787e5af63c78fa2d75209941a9a977
mzagr <mzagrebam@gmail.com>
2024-05-04 21:02:06 +0300

    add endpoints to login and get securities data. connect it to main app

---
+++ app/auth/views.py
@@ -0,0 +1,38 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth.config import auth_settings
+from app.auth.schemas import Token
+from app.auth.utils import create_access_token
+from app.auth.user_operations import authenticate_user, get_current_user
+from app.db.config import DB
+from app.users.schemas import UserSchema
+
+router = APIRouter(tags=['Auth'], prefix='/auth')
+
+
+@router.post("/login/")
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        session: AsyncSession = Depends(DB.session_dependency)
+) -> Token:
+    user = await authenticate_user(session=session,
+                                   username=form_data.username,
+                                   password=form_data.password)
+
+    access_token_expires = timedelta(minutes=auth_settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.get("/users/me/", response_model=UserSchema)
+async def read_users_me(
+        current_user: Annotated[UserSchema, Depends(get_current_user)],
+):
+    return current_user
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from app.users.views import router as user_router
+from app.auth.views import router as auth_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(auth_router)
 
 
 @app.get('/')
