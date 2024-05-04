e8b89d0691e6cb249f28ec3f6b16ee801342a069
mzagr <mzagrebam@gmail.com>
2024-05-04 20:58:16 +0300

    install dependencies and add config file for getting keys

---
+++ app/auth/config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+
+CURRENT_DIR = Path(__file__).parent
+
+
+def get_key(filepath: str | Path) -> bytes:
+    with open(filepath, "rb") as key_file:
+        key = key_file.read()
+        return key
+
+
+class AuthSettings(BaseModel):
+    PRIVATE_KEY_PATH: Path = CURRENT_DIR / 'keys' / 'jwt-private.pem'
+    PUBLIC_KEY_PATH: Path = CURRENT_DIR / 'keys' / 'jwt-public.pem'
+
+    PRIVATE_KEY: bytes = get_key(filepath=PRIVATE_KEY_PATH)
+    PUBLIC_KEY: bytes = get_key(filepath=PUBLIC_KEY_PATH)
+    ALGORITHM: str = 'RS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+
+
+auth_settings = AuthSettings()
